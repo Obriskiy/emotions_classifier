2c1523da18caf124727b3d206cb2d77eac730ac0
Dmitry Obriskiy <Dmitry.Obriskiy@nice.com>
2019-03-28 17:21:12 +0100

    updated model

--- app/server.py
+++ app/server.py
@@ -8,7 +8,7 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-model_file_url = 'https://drive.google.com/uc?export=download&id=1z6OPsvilMqyZYsVbeFJz9vRrOPDZmpJD'
+model_file_url = 'https://drive.google.com/uc?export=download&id=1zAGEmNICxs4jxa1pDrE9QQ2zEDaCwIb5'
 model_file_name = 'model'
 classes = ['afraid', 'angry', 'disgusted', 'happy', 'sad', 'surprised']
 path = Path(__file__).parent
